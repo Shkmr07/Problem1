483e4758dbe04be3f2cab1bc88d7f841eb2af16c
Ayush Kumar Singh <shkmrsingh@gmail.com>
2022-11-30 15:12:56 +0530

    palindrom1

---
+++ palindrom.js
@@ -0,0 +1,13 @@
+let r=num.toString();
+let a="";
+    for(i=r.length-1;i>=0;i--){
+        a+=r[i];
+    }
+    if(a===r){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
+
+
